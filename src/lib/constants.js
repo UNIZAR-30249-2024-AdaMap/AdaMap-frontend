93fbbe2a8232bbc92bf3e5336381ea7b7fc69607
marinalamiel <marinalamielvi@gmail.com>
2024-04-14 20:19:06 +0200

    filters

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -14,5 +14,60 @@ export const NavBarItems = [
   {
     title: 'Mapa',
     href: '/map'
+  },
+  {
+    title: 'Buscar & Filtrar',
+    href: '/filters'
+  }
+]
+
+
+export const Category = [
+  {
+    title: 'Aula', 
+    value: 'aula'
+  },
+  {
+    title: 'Laboratorio', 
+    value: 'laboratorio'
+  },
+  {
+    title: 'Seminario', 
+    value: 'seminario'
+  },
+  {
+    title: 'Sala Común', 
+    value: 'sala-comun'
+  },
+  {
+    title: 'Despacho', 
+    value: 'despacho'
+  }
+]
+
+export const Planta = [
+  {
+    title: 'Planta 0', 
+    value: 'planta-0'
+  },
+  {
+    title: 'Planta 1', 
+    value: 'planta-1'
+  },
+  {
+    title: 'Planta 2', 
+    value: 'planta-2'
+  },
+  {
+    title: 'Planta 3', 
+    value: 'planta-3'
+  },
+  {
+    title: 'Planta 4', 
+    value: 'planta-4'
+  },
+  {
+    title: 'Planta 5', 
+    value: 'planta-5'
   }
 ]
